a1cb03a06237533fb7a4191b9951b0ec5a7123aa
mayumiasio <mayumayumayumi.11@gmail.com>
2019-07-28 12:52:32 +0900

    chat space  Completed fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
     config.public_file_server.headers = {
       'Cache-Control' : 'public, max-age=172800'
     }
-  els
+  else
     config.action_controller.perform_caching = false
 
     config.cache_store = :null_store
